f6861a87ec284920ee15e9e910cc819aad8dbe86
glenoh88 <glenoh88@icloud.com>
2015-10-10 02:37:59 -0700

    initial check in

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'install_mongodb'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures install_mongodb'
+long_description 'Installs/Configures install_mongodb'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: install_mongodb
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
